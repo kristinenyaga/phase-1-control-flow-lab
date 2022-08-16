0afd86577c8cce1a2215801eb7b80d6410bc165b
krisine <kristine.nyaga@student.moringaschool.com>
2022-08-16 15:40:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if(feet<=400){
+    return `This one is on me!`
+  }
+  else if(feet>2000 && feet<2500){
+    return `I will gladly take your thirty bucks.`
+  }
+  else{
+    return `No can do.`
+  }
 }
+scuberGreetingForFeet(199)
+scuberGreetingForFeet(2001)
+scuberGreetingForFeet(2501)
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  // Write your code here!'
+ 
+  return (city === 'NYC')?'Ok, sounds good.':'No go.'
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
+ternaryCheckCity('NYC')
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return `Thank you.`
+      break;
+    case 'thanks for everything':
+      return 'Bye.'  
+  }
+  
+}
+switchOnCharmFromTip('generous')
